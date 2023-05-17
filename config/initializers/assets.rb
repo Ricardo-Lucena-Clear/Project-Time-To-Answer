42bc98b3040857bdedffc68bb46db15009db7b38
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-05-17 12:34:41 -0300

    Porting the Gentelella theme to User (Part 2/2)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -18,4 +18,14 @@ Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_ba
 
 # /lib/assets
 Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css 
-                                                  custom.js custom.css              )
+                                                  custom.js custom.css  img.jpg            )
+
+# /vendor/assets
+Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.js )
+
+
+
+
+
+
+
