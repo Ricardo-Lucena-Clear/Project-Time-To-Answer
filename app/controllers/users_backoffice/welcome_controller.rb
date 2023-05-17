7bdd2e5edae6234abffbe506833906051e977877
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-05-17 11:36:51 -0300

    Porting SBAdmin theme to Admin and fixing ALL project

--- app/controllers/users_backoffice/welcome_controller.rb
+++ app/controllers/users_backoffice/welcome_controller.rb
@@ -1,6 +1,4 @@
 class UsersBackoffice::WelcomeController < UsersBackofficeController
-  before_action :authenticate_user!
-  layout 'users_backoffice'
-  def index
-  end
+    def index
+    end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
+# require 'rails/all'
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
@@ -20,16 +21,14 @@ Bundler.require(*Rails.groups)
 module TimeToAnswer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 5.2
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
-    config.generators.system_tests= nil
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end
