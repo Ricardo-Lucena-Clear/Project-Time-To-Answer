d7960ed318cc16a79035b6498ac6566c7c0e6933
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-05-19 12:10:53 -0300

    Incrementing the i18n Models(admin, questions and subjects)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
     def translate_field(object = nil, method = nil)
-    (object && method) ? object.model.human_attribute_name(method) :
-   "Informe os parâmetros corretamente!!!"
+    (object && method) ? object.model.human_attribute_name(method) : "Informe os parâmetros corretamente!!!"
     end
    end
