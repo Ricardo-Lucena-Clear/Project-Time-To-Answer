c891bcda442071028ac64bd7c24813edb92c7650
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-05-22 09:36:35 -0300

    A dash of metaprogramming in the Devise User login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,9 @@
 class ApplicationController < ActionController::Base
 # Set Layout
   layout :layout_by_resource
-  private
-    def layout_by_resource
-    if devise_controller? && resource_class == Admin
-        "admin_devise"
-    else
-        "application"
-        end
+  protected
+  def layout_by_resource
+    devise_controller? ? "#{resource_class.to_s.downcase}_devise"
+   : "application"
     end
 end
