dca8a1e3d6a92f6b54f06f1f16af2ab939587fc6
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-05-22 12:10:47 -0300

    Making the search work

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,5 +1,8 @@
 class Site::SearchController < SiteController
     def questions
-    @questions = Question.includes(:answers).page(params[:page])
-    end
+        @questions = Question.includes(:answers)
+        .where("lower(description) LIKE ?",
+        "%#{params[:term].downcase}%")
+        .page(params[:page])
+        end
    end
