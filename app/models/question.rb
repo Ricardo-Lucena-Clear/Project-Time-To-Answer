9908f5e7e06cc05a97d0d9aa57fa02c3a0eb16f4
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-05-24 12:25:25 -0300

    Add Admin stats

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,10 @@ class Question < ApplicationRecord
   accepts_nested_attributes_for :answers, reject_if:
  :all_blank, allow_destroy: true
 
+
+ after_create :set_statistic
+
+ 
  paginates_per 5
 
 
@@ -21,5 +25,12 @@ class Question < ApplicationRecord
  scope :last_questions, ->(page) {
  includes(:answers, :subject).order('created_at desc').page(page)
  }
+ private
+
+
+  def set_statistic
+    AdminStatistic.set_event(AdminStatistic::EVENTS[:total_questions])
+    
   end
+end
  
